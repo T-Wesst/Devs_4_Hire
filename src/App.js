968159b5bc038fc906b8ddda58556f3dae9518d5
T-Wesst <code.daddy17@gmail.com>
2019-02-04 14:43:59 -0800

    npm install bootstrap and render jobseeker component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import JobSeeker from './components/jobSeeker/JobSeeker';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import Header from './components/header/Header.js';
 class App extends Component {
   render() {
@@ -11,6 +12,11 @@ class App extends Component {
           name="Karren Smith"
           email="karren@gmail.com"
           phone="818-255-2121"
+        />
+        <JobSeeker
+          name="mike Smith"
+          email="mike@gmail.com"
+          phone="818-123-2121"
         />
       </div>
     );
