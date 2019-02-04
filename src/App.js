3e1bbfd4394c995b526b65c34890e9b7e2f0fb5b
T-Wesst <code.daddy17@gmail.com>
2019-02-04 14:57:58 -0800

    add bootstrap classes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import Header from './components/header/Header.js';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <JobSeeker
-          name="Karren Smith"
-          email="karren@gmail.com"
-          phone="818-255-2121"
-        />
-        <JobSeeker
-          name="mike Smith"
-          email="mike@gmail.com"
-          phone="818-123-2121"
-        />
+      <div className="App">
+        <div className="container">
+          <Header branding="Devs_4_Hire" />
+          <JobSeeker
+            name="Karren Smith"
+            email="karren@gmail.com"
+            phone="818-255-2121"
+          />
+          <JobSeeker
+            name="mike Smith"
+            email="mike@gmail.com"
+            phone="818-123-2121"
+          />
+        </div>
       </div>
     );
   }
