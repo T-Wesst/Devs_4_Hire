6369bf8ab34afc9e74830bdb112d57b771fecbbd
T-Wesst <code.daddy17@gmail.com>
2019-02-04 15:22:59 -0800

    important render Jobseekers component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import JobSeeker from './components/jobSeeker/JobSeeker';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import JobSeekers from './components/jobSeeker/JobSeekers';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header.js';
 class App extends Component {
   render() {
@@ -9,16 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <Header branding="Devs_4_Hire" />
-          <JobSeeker
-            name="Karren Smith"
-            email="karren@gmail.com"
-            phone="818-255-2121"
-          />
-          <JobSeeker
-            name="mike Smith"
-            email="mike@gmail.com"
-            phone="818-123-2121"
-          />
+          <JobSeekers />
         </div>
       </div>
     );
