4b04fe95b94b3f23f87f949a6766b09cd9d5cb96
T-Wesst <code.daddy17@gmail.com>
2019-02-07 15:24:47 -0800

    import provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
 import JobSeekers from './components/jobSeeker/JobSeekers';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header.js';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from './context';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header branding="Devs_4_Hire" />
-          <JobSeekers />
+      <Provider>
+        <div className="App">
+          <div className="container">
+            <Header branding="Devs_4_Hire" />
+            <JobSeekers />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
