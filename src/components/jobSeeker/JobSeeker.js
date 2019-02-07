d250dd002d93a47ce5145b80f45bd4ead707df21
T-Wesst <code.daddy17@gmail.com>
2019-02-07 15:25:59 -0800

    import consumer, loop through applicants and render Jobseeker component with props

--- src/components/jobSeeker/JobSeeker.js
+++ src/components/jobSeeker/JobSeeker.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import { Consumer } from '../../context';
 // Subclass component
 export default class JobSeeker extends Component {
   render() {
     // destructuring props
-    const { name, email, phone } = this.props;
+    const { name, email, phone, linkedIn } = this.props;
     return (
-      <div className="card card-body">
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-          <li className="list-group-item">Name: {name}</li>
-        </ul>
-      </div>
+      <Consumer>
+        {() => {
+          return (
+            <div className="card card-body">
+              <ul className="list-group">
+                <li className="list-group-item">Email: {email}</li>
+                <li className="list-group-item">Phone: {phone}</li>
+                <li className="list-group-item">Name: {name}</li>
+                <li className="list-group-item">LinkedIn: {linkedIn}</li>
+              </ul>
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
--- src/components/jobSeeker/JobSeekers.js
+++ src/components/jobSeeker/JobSeekers.js
@@ -1,45 +1,26 @@
 import React, { Component } from 'react';
 import JobSeeker from './JobSeeker';
-
+import { Consumer } from '../../context';
 export default class JobSeekers extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'John Doe',
-          email: 'jdoe@gmail.com',
-          phone: '818-818-8118'
-        },
-        {
-          id: 2,
-          name: 'Karen Smith',
-          email: 'karen@gmail.com',
-          phone: '818-224-3234'
-        },
-        {
-          id: 3,
-          name: 'Mike Myers',
-          email: 'mike@gmail.com',
-          phone: '818-211-1122'
-        }
-      ]
-    };
-  }
   render() {
-    const { contacts } = this.state;
     return (
-      <div>
-        {contacts.map(applicant => (
-          <JobSeeker
-            key={applicant.id}
-            name={applicant.name}
-            email={applicant.email}
-            phone={applicant.phone}
-          />
-        ))}
-      </div>
+      <Consumer>
+        {applicants => {
+          return (
+            <React.Fragment>
+              {applicants.map(applicant => (
+                <JobSeeker
+                  key={applicant.id}
+                  name={applicant.name}
+                  email={applicant.email}
+                  phone={applicant.phone}
+                  linkedIn={applicant.linkedIn}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
