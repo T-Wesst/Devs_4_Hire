a7c089ea6bbb0d5667057ba9deb2aa454c42d444
T-Wesst <code.daddy17@gmail.com>
2019-02-08 15:11:59 -0800

    add icons

--- src/components/jobSeeker/JobSeeker.js
+++ src/components/jobSeeker/JobSeeker.js
@@ -4,21 +4,36 @@ import { Consumer } from '../../context';
 export default class JobSeeker extends Component {
   render() {
     // destructuring props
-    const { name, email, phone, linkedIn, image } = this.props;
+    const { name, email, phone, linkedIn, image, title, bio, github, portfolio } = this.props;
     return (
       <Consumer>
         {() => {
           return (
-            <div className="card card-body">
-              <ul className="list-group">
-                <li>
-                  <img src={image} alt="" />
-                </li>
-                <li className="list-group-item">Email: {email}</li>
-                <li className="list-group-item">Phone: {phone}</li>
-                <li className="list-group-item">Name: {name}</li>
-                <li className="list-group-item">LinkedIn: {linkedIn}</li>
-              </ul>
+            <div className="card-body">
+              <div className="card" style={{ width: '18rem' }}>
+                <img src={image} alt={name} className="img-card-top" />
+                <div class="card-body">
+                  <h5 class="card-title">{name}
+                    <a href={linkedIn} rel="noopener noreferrer" target="_blank">
+                      <i class="fab fa-linkedin" />
+                    </a>
+                    <a href={github}>
+                      <i class="fab fa-github" />
+                    </a>
+                    <a href={portfolio}>
+                      <i class="fas fa-folder-open" />
+                    </a>
+                  </h5>
+                  <h6 class="card-subtitle mb-2 text-muted">{title}</h6>
+                  <p class="card-text">Bio: {bio}</p>
+                  <a href={phone} className="btn btn-primary">
+                    <i className="fas fa-phone" /> Contact
+                  </a>
+                  <a href={`mailto:${email}?Subject=Hello ${name}`} className="btn btn-danger">
+                    <i className="fas fa-envelope" />
+                  </a>
+                </div>
+              </div>
             </div>
           );
         }}
--- src/components/jobSeeker/JobSeekers.js
+++ src/components/jobSeeker/JobSeekers.js
@@ -11,8 +11,12 @@ export default class JobSeekers extends Component {
               {applicants.map(applicant => (
                 <JobSeeker
                   key={applicant.id}
+                  title={applicant.title}
                   image={applicant.image}
+                  github={applicant.github}
+                  bio={applicant.bio}
                   name={applicant.name}
+                  portfolio={applicant.portfolio}
                   email={applicant.email}
                   phone={applicant.phone}
                   linkedIn={applicant.linkedIn}
